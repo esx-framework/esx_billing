cdb396d6822d791a5ca13c52d3407637b391978c
Zerio <neoboij@gmail.com>
2024-06-24 20:29:14 +0000

    feat: trigger event upon paying bill

--- client/main.lua
+++ client/main.lua
@@ -16,9 +16,15 @@ function ShowBillsMenu()
 			end
 
 			ESX.OpenContext("right", elements, function(menu,element)
-				ESX.TriggerServerCallback('esx_billing:payBill', function()
+				local billId = element.billId
+
+				ESX.TriggerServerCallback('esx_billing:payBill', function(resp)
+					if resp == true then
+						TriggerEvent("esx_billing:paidBill", billId)
+					end
+
 					ShowBillsMenu()
-				end, element.billId)
+				end, billId)
 			end)
 		else
 			ESX.ShowNotification(TranslateCap('no_invoices'))
